7de6146e3161022e9c96f228d18496b786165b39
pablo <insomnionebulous@gmail.com>
2022-03-23 20:30:50 -0300

    arreglo estilos

--- src/components/common/cards/ItemCard.jsx
+++ src/components/common/cards/ItemCard.jsx
@@ -1,24 +1,43 @@
 import { Link } from "react-router-dom"
 import { HeartIcon } from "../../../assets/icons"
 
-export const ItemCard = () => {
+export const ItemCard = ({
+  title,
+  imageUrl,
+  price,
+  onSale,
+  currency,
+  maxInstallments,
+  freeShipping,
+}) => {
+  const finalPrice = Math.floor((price - (price * onSale) / 100) * 100) / 100
+
+  const installmentsPrice = Math.round(finalPrice / maxInstallments)
   return (
     <Link to="product/id" className="item-card hover-shadow-1">
       <div className="item-card-img">
-        <img src="https://http2.mlstatic.com/D_Q_NP_786472-MLU46077199983_052021-AB.webp" />
+        <img src={imageUrl} alt="item" />
         <button className="item-heart-icon">
           <HeartIcon />
         </button>
       </div>
       <div className="item-card-description">
-        <p className="item-price-without-discount">$ 4.400</p>
+        {onSale && (
+          <p className="item-price-without-discount">
+            {currency + " " + price}
+          </p>
+        )}
         <div className="item-price">
-          <p>$ 4.000 </p>
-          <p className="item-discount">10% OFF</p>
+          <p>{currency + " " + finalPrice}</p>
+          {onSale && <p className="item-discount">{onSale}% OFF</p>}
         </div>
-        <p className="item-installments">10x $ 400 sin interés</p>
-        <p className="item-free-shipping">Envio Gratis</p>
-        <p className="item-title">Una cuadernola toda wapa</p>
+        {maxInstallments && (
+          <p className="item-installments">
+            {maxInstallments}x $ {installmentsPrice} sin interés
+          </p>
+        )}
+        {freeShipping && <p className="item-free-shipping">Envio Gratis</p>}
+        <p className="item-title">{title}</p>
       </div>
     </Link>
   )
--- src/components/common/items-gallery/HomeGallery.jsx
+++ src/components/common/items-gallery/HomeGallery.jsx
@@ -1,19 +1,20 @@
 import React from "react"
 import { ItemCard } from "../cards/ItemCard"
 
-export const HomeGallery = ({ title }) => {
-	return (
-		<div className="gallery-ctn">
-			<h2 className="gallery-title">{title}</h2>
-			<div className="gallery-items-ctn">
-				<ItemCard />
-				<ItemCard />
-				<ItemCard />
-				<ItemCard />
-				<button className="gallery-item-next">
-					<i className="fi fi-angle-right"></i>
-				</button>
-			</div>
-		</div>
-	)
+export const HomeGallery = ({ title, products, isLoading }) => {
+  return (
+    <div className="gallery-ctn">
+      <h2 className="gallery-title">{title}</h2>
+      <div className="gallery-items-ctn">
+        {!isLoading &&
+          products.map((product) => (
+            <ItemCard {...product} key={product.title} />
+          ))}
+
+        <button className="gallery-item-next">
+          <i className="fi fi-angle-right"></i>
+        </button>
+      </div>
+    </div>
+  )
 }
