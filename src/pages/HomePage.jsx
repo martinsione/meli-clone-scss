55481078b05a46dc44b5468b37b9fd15ccc5f3fa
pablo <insomnionebulous@gmail.com>
2022-03-24 15:20:02 -0300

    correcciones

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from "react"
-
 import { Carrousel } from "../components/Carrousel"
 import { CategoriesCardGallery } from "../components/cards-galleries/CategoriesCardGallery"
 import { HomeGallery } from "../components/cards-galleries/HomeGallery"
 import { PaymentMethodsInfo } from "../components/PaymentMethodsInfo"
 import StoresCardGallery from "../components/cards-galleries/StoresCardGallery"
-
-import bannerImg from "../assets/images/banners/banner1.jpg"
 import { ShoppingInfo } from "../components/ShoppingInfo"
 import { useGetHomeInfo } from "../hooks/useGetHomeInfo"
+
+import bannerImg from "../assets/images/banners/banner1.jpg"
 
 export const HomePage = ({ country }) => {
   const lastSearch = localStorage.getItem("lastSearch")
